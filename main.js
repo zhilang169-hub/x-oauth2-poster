f7f8f58641329a23c3b1adbd37b45fda592af35f
zhilang169-hub <zhilang169@gmail.com>
2026-05-30 10:45:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("OAuth2 poster start");
+import OpenAI from "openai";
+
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
+
+async function main() {
+  console.log("OpenAI connected");
+
+  const response = await client.responses.create({
+    model: "gpt-5",
+    input: "Say hello"
+  });
+
+  console.log(response.output_text);
+}
+
+main().catch(console.error);
